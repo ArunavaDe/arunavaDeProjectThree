38ab2b040daf85029c1fa96f556163a71ff031aa
arunavaDe <arunava.de@gmail.com>
2019-08-23 14:58:11 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,340 @@
+const food = {
+    spicy: [
+    {
+        title:'Mutton/ Chicken Biryani',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'notDate',
+        moodToday: 'rd'
+    },
+    
+    {
+        title:'Mutton/ Chicken Do Piaza',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'notDate',
+        moodToday: 'rs'
+    },
+
+    {
+        title:'Chicken/ Paneer Tikka Masala',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'notDate',
+        moodToday: 'ff'
+    },
+
+    {
+        title:'Masala Dosa',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'notDate',
+        moodToday: 'sm'
+    },
+
+    {
+        title:'Chicken Chettinadu',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'notDate',
+        moodToday: 'rs'
+    },
+
+    {
+        title:'Pork/ Chicken/ Mutton Vindaloo',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'date',
+        moodToday: 'rd'
+    },
+
+    {
+        title:'Podi Idlee with Sambar and assorted Chutney',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'date',
+        moodToday: 'rs'
+    },
+
+    {
+        title:'Beef Cherulli Masala ',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'date',
+        moodToday: 'ff'
+    },
+
+    {
+        title:'Yakhni (Yogurt Lamb Curry)',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'date',
+        moodToday: 'sm'
+    },
+
+    {
+        title:'Kerala Paratha and Fish curry',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'notDate',
+        moodToday: 'ff'
+    },
+
+    {
+        title:'Garlic Naan and Paneer Pasinda',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'notDate',
+        moodToday: 'sm'
+    },
+
+    {
+        title:'Chicken/ Mutton / Paneer Kaati roll',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'notDate',
+        moodToday: 'rs'
+    },
+
+    {
+        title:'Meat / Vegetable Qurma',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'notDate',
+        moodToday: 'rd'
+    },
+
+    {
+        title:'Vegetable Uttapam with Chutney and Sambar',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'date',
+        moodToday: 'rs'
+    },
+
+    {
+        title:'Kerala Fish Ghee Roast with Rice',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'date',
+        moodToday: 'rd'
+    },
+
+    {
+        title:'Jeera Aloo with Chapati',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'date',
+        moodToday: 'sm'
+    },
+
+    {
+        title:'Bengali Fish Kalia Curry with Rice',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'date',
+        moodToday: 'ff'
+    }
+
+    ],
+
+    mild : [
+
+    {
+        title:'Butter Chicken/Paneer',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'date',
+        moodToday: 'ff'
+    },
+
+    {
+        title:'Kothu Paratha',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'date',
+        moodToday: 'sm'
+    },
+
+    {
+        title:'Mulligatawny',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'notDate',
+        moodToday: 'rs'
+    },
+
+    {
+        title:'Shahi Paneer and Naan',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'notDate',
+        moodToday: 'rd'
+    },
+
+    {
+        title:'Mutton/ Chicken Curry with Basmati Rice',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'date',
+        moodToday: 'rd'
+    },
+
+    {
+        title:'Chana Masala with Naan bread',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'notDate',
+        moodToday: 'rd'
+    },
+    
+    {
+        title:'Saag Paneer',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'notDate',
+        moodToday: 'rs'
+    },
+
+    {
+        title:'Aloo Gobi Masala with Kulcha',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'notDate',
+        moodToday: 'ff'
+    },
+
+    {
+        title:'Basanti Pulav with Chickpea masala',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'notDate',
+        moodToday: 'sm'
+    },
+
+    {
+        title:'Rogan Josh with Butter Naan',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'notDate',
+        moodToday: 'rs'
+    },
+
+    {
+        title:'Fish Fry in Indian Mustard sauce',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'date',
+        moodToday: 'rd'
+    },
+
+    {
+        title:'Methi Paratha with Palak Paneer',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'date',
+        moodToday: 'rs'
+    },
+
+    {
+        title:'Bengali Lentil soup with Rice and Fried Fish',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'date',
+        moodToday: 'ff'
+    },
+
+    {
+        title:'Poha and Crunchy potato fry',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'date',
+        moodToday: 'sm'
+    },
+
+    {
+        title:'Goan Fish Curry with Rice',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'notDate',
+        moodToday: 'ff'
+    },
+
+    {
+        title:'Samosa Chaat',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'notDate',
+        moodToday: 'sm'
+    },
+
+    {
+        title:'Chicken/ Mutton Haryali Kebab',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'notDate',
+        moodToday: 'rs'
+    },
+
+    {
+        title:'Sambar Sadam with Pappadam',
+        mealQuantity: 'quickBite',
+        dateOrNot: 'notDate',
+        moodToday: 'rd'
+    },
+
+    {
+        title:'Roti and Slow Cooked Bengali Goat Curry',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'date',
+        moodToday: 'rs'
+    },
+
+    {
+        title:'Chicken Tikka Masala with Naan/rice',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'date',
+        moodToday: 'rd'
+    },
+
+    {
+        title:'Vegetable/ Chicken Jalfrezi',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'date',
+        moodToday: 'sm'
+    },
+
+    {
+        title:'Beef/ Mutton Bhuna with a side of Tandoori Chicken',
+        mealQuantity: 'fullMeal',
+        dateOrNot: 'date',
+        moodToday: 'ff'
+    }
+
+    ]
+};
+
+$(document).ready(function(){
+
+    const getRandomItem = function(array) {
+        const random = Math.floor(Math.random() * array.length);
+        return array[random];
+    }
+
+    $('form').on('submit', function(event){
+        event.preventDefault();
+
+        $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+
+        
+        const userSpiceLevel = $('input[name="spiceChoice"]:checked').val();
+        
+        const userQuantity = $('input[name="mealQuantity"]:checked').val();
+        
+        const userDate = $('input[name="dateOrNot"]:checked').val();
+        
+        const userMood = $('input[name="moodToday"]:checked').val();
+        
+    
+        
+        if (userDate === undefined || userSpiceLevel === undefined || userQuantity === undefined || userMood === undefined) {
+            
+            $('.results').html(`<h2>Oops! Looks like you missed something. I can help you better if you answer all the questions!</h2> <input type="reset" value="try it again">`);
+            
+        } 
+        else {
+        
+
+        const foodArray = food[userSpiceLevel];
+        
+        
+        const filteredArray = [];
+        for( let i = 0; i < foodArray.length; i++) {
+            if((userQuantity === foodArray[i].mealQuantity) && (userDate === foodArray[i].dateOrNot) && (userMood === foodArray[i].moodToday)) {
+                filteredArray.push(foodArray[i].title);
+            }
+        }
+        
+        
+        const finalResult = getRandomItem(filteredArray);
+        
+        
+        $('.results').html(`<h2>You might want to try ${finalResult}</h2> <input type="reset" value="try it again">`);
+
+        }
+    })
+    
+    $('form').on('reset', function(){
+        
+        $('.results').html(``);
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+        
+    });
+    
+})
